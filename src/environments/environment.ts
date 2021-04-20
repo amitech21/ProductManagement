b9b8765d653c5070cedcbb2acb58b19bf94b2e92
amitech21 <a.panchal21@gmail.com>
2021-04-20 19:23:58 +0200

    angular commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   googleID: '443817139519-5bjd35c0robn5k8c65g643gb9qe4lmoo.apps.googleusercontent.com',
-  webAppEndPoint: 'http://localhost:8080',
+  webAppEndPoint: 'https://65.1.184.255:8443',
   firebaseAPIKey: 'AIzaSyAHzxxfhxqs1pBA5Kl5de4pmCrf4Ph8Bqc'
 };
 
